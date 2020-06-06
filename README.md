8cb9ae8bc24a0757cba5f0558d7d00758700a50a
Cisco D <46197605+cisco2d@users.noreply.github.com>
2020-06-06 12:10:22 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Boilerplate for 3js
+
+```js
+git clone https://github.com/cisco3d/3js-boiler.git
+cd 3js-boiler
+npm install -g typescript
+npm install
+npm run dev
+```
---
+++ src/server/server.ts
@@ -0,0 +1,28 @@
+import http from 'http';
+import path from 'path';
+import express from 'express';
+
+const port: number = 3000
+
+class App {
+    private server: http.Server
+    private port: number
+
+    constructor(port: number) {
+        this.port = port
+        const app = express()
+        app.use(express.static(path.join(__dirname, '../client')))
+        app.use('/build/three.module.js', express.static(path.join(__dirname, '../../node_modules/three/build/three.module.js')))
+        app.use('/jsm/controls/OrbitControls', express.static(path.join(__dirname, '../../node_modules/three/examples/jsm/controls/OrbitControls.js')))
+
+        this.server = new http.Server(app);
+    }
+
+    public Start() {
+        this.server.listen(this.port, () => {
+            console.log( `Server listening on a port ${this.port}.` )
+        })
+    }
+}
+
+new App(port).Start()
